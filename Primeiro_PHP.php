7a4ef4c28c9a724be2957a895459c83271e30bc8
Luís Fernando de Freitas <freitasluisito508@gmail.com>
2025-07-04 17:07:14 -0300

    Create Primeiro_PHP.php
    primeiro exemplo php

---
+++ Primeiro_PHP.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Primeiro Programa PHP</title>
+</head>
+<body>
+    <?php
+        echo "<h1>Hello world, PHP-8!</h1>";
+        phpinfo();
+    ?>
+    <br><br><address align="center">
+        Luís Fernando / Estudante / Técnico Desenvolvimento de Sistemas
+    </address>
+</body>
+</html>
