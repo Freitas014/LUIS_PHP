7d47dfa90ed86dcac1e591d8e65f0ff1a78c8a21
Luís Fernando de Freitas <freitasluisito508@gmail.com>
2025-07-04 17:07:23 -0300

    Create Saida.php
    print php

---
+++ Saida.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<!-- Php 8 não funciona o \n-->
+    <?php
+        print "teste<br>";
+        print "Olá, Mundo<br>";
+        print "Escape 'chars' são os MENOS como em C<br>";
+        print "Você pode ter quebras de linhas em uma string. <br>";
+        print 'Uma strin pode usar "aspa-duplas". Isso é muito legal!<br>';
+        print 'Ainda pode-se usar aspas simples dessa forma It\'s cool!<br>';
+    ?>
+    <!-- ECHO Também não funciona \n-->
+    <br><br>
+    <?php
+        /*echo "teste<br>";
+        echo "Olá, Mundo<br>";
+        echo "Escape 'chars' são os MENOS como em C<br>";
+        echo "Você pode ter quebras de linhas em uma string. <br>";
+        echo 'Uma strin pode usar "aspa-duplas". Isso é muito legal!<br>';
+        echo 'Ainda pode-se usar aspas simples dessa forma It\'s cool!<br>';
+        */
+    ?>
+    <br><br><address align="center">
+        Luís Fernando / Estudante / Técnico Desenvolvimento de Sistemas
+    </address>
+</body>
+</html>
