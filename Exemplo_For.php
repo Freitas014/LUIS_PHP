6714c614ceed147c9ff9f32e998f6c439581cab3
Luís Fernando de Freitas <freitasluisito508@gmail.com>
2025-07-11 17:12:13 -0300

    Create Exemplo_For.php

---
+++ Exemplo_For.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>For</title>
+</head>
+<body>
+    <?php
+        for ($i = 0; $i < 10; $i++) {
+            print "O quadrado de $i é " . $i * $i . "<br>";
+        }
+    ?>
+    <br><br><address align="center">
+        Luís Fernando / Estudante / Técnico Desenvolvimento de Sistemas
+    </address>
+</body>
+</html>
