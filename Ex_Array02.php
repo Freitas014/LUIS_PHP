0a18cd418caa9530181c275f9974a1b18c1679e7
Luís Fernando de Freitas <freitasluisito508@gmail.com>
2025-07-14 17:12:35 -0300

    Update Ex_Array02.php
    removido erros

--- Ex_Array02.php
+++ Ex_Array02.php
@@ -10,16 +10,16 @@
     $estados = array ("PR", "SC", "RS", "SP", "RJ", "MG", "BA", "RN", "AM");
     echo "ORIGINAL: ";
     echo "<hr>STRTOLOWER: Converte uma string para minúsculas <br>";
-    for ($i = 0; $i < count($estados); $i++);{
+    for ($i = 0; $i < count($estados); $i++) { // Removido o ponto e vírgula
         $estados[$i] = strtolower($estados[$i]);
     }
     echo "STROLOWER: "; print_r ($estados);
-    echo "<hr> SHIFT: Retira o primeiro elemntos de um array<br>";
+    echo "<hr> SHIFT: Retira o primeiro elemento de um array<br>";
     $rotaciona = array_shift($estados);
     echo "SHIFT: "; print_r ($estados);
     echo "<hr>POP: Extrai um elemento do final do array<br>";
     array_pop($estados);
-    echo "PUSH: "; print_r ($estados);
+    echo "POP: "; print_r ($estados);
     echo "<hr>REVERSE: Retorna um array com os elementos na ordem inversa<br>";
     $inverso = array_reverse($estados);
     echo "REVERSE: "; print_r($inverso);
@@ -30,5 +30,8 @@
     $dividir = array_slice($estados, 1, 2);
     echo "SLICE :"; print_r ($dividir); echo "<br>";
     ?>
+    <br><br><address align="center">
+        Luís Fernando / Estudante / Técnico Desenvolvimento de Sistemas
+    </address>
 </body>
 </html>
