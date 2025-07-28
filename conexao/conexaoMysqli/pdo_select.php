d31ee5ca74257980064b6fbba2a2ddf024719320
Luís Fernando de Freitas <freitasluisito508@gmail.com>
2025-07-28 17:05:43 -0300

    Create pdo_select.php

---
+++ conexao/conexaoMysqli/pdo_select.php
@@ -0,0 +1,14 @@
+<?php
+$endereco = "localhost";
+$usuario = "root";
+$senha = "";
+$banco = "empresa";
+
+try{
+    $conexao = new PDO("mysql:host=$endereco; dbname=$empresa; ");
+}
+catch {
+
+}
+
+?>
