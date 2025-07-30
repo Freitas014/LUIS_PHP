b18b64848a791bd59f6a01a46969b688616d1b48
Luís Fernando de Freitas <freitasluisito508@gmail.com>
2025-07-30 16:05:36 -0300

    Create Menu.php

---
+++ conexao/conexaoPdo/Menu.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CRUD - Menu</title>
+</head>
+<body>
+    <h1> Tela Início - CRUD
+</body>
+</html>
