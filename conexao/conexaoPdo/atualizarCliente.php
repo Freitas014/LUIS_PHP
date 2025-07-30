9664537d29ce6c7567ee83bc6029c1a6fcf526bb
Luís Fernando de Freitas <freitasluisito508@gmail.com>
2025-07-30 17:07:21 -0300

    Update atualizarCliente.php
    estilizada

--- conexao/conexaoPdo/atualizarCliente.php
+++ conexao/conexaoPdo/atualizarCliente.php
@@ -33,7 +33,7 @@ if ($idCliente && is_numeric($idCliente)){
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Atualizar Cliente</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="style_menu.css">
     <script>
         function habilitarEdicao(campo) {
             document.getElementById(campo).removeAttribute("readonly");
@@ -42,15 +42,15 @@ if ($idCliente && is_numeric($idCliente)){
     </script>
 </head>
 <body>
-    <h2>Atualizar Cliente</h2>
-
+    <main>
+    <h1>Atualizar Cliente</h1>
     <!-- Se houver erro, exibe a mensagem e o campo de busca-->
     <?php if ($msgErro): ?>
         <p style="color:red;"> <?= htmlspecialchars ($msgErro) ?> </p>
         <form action="atualizarCliente.php" method ="get">
             <label for="idCliente">ID do Cliente:</label>
-            <input type="number" id="id" name="id" required>
-            <button type="submit">Buscar</button>
+            <input class="form-control" type="number" id="id" name="id" placeholder="Insira o ID do cliente" required>
+            <button id="btn" class="btn btn-primary"type="submit">Buscar</button>
         </form>
     <?php else: ?>
 
@@ -59,13 +59,66 @@ if ($idCliente && is_numeric($idCliente)){
         <input type="hidden" name="id_cliente" value ="<?=htmlspecialchars($cliente['id_cliente'])?>">
 
         <label for="telefone">Telefone:</label>
-        <input type="text" id="nome" name="nome" value ="<?=htmlspecialchars($cliente['telefone'])?>" readonly onclick="habilitarEdicao('telefone')">
+        <input class="form-control" type="text" id="telefone" name="telefone" value ="<?=htmlspecialchars($cliente['telefone'])?>" readonly onclick="habilitarEdicao('telefone')">
         
         <label for="email">Email:</label>
-        <input type="email" id="email" name="email" value ="<?=htmlspecialchars($cliente['email'])?>" readonly onclick="habilitarEdicao('email')">
-        <button type="submit">Atualizar Cliente</button>
+        <input class="form-control" type="email" id="email" name="email" value ="<?=htmlspecialchars($cliente['email'])?>" readonly onclick="habilitarEdicao('email')">
+        <button id="btn" class="btn btn-primary" type="submit">Atualizar Cliente</button>
     </form>
     <?php endif; ?>
+    </main>
 
+    <div id="sidebar" class="d-flex flex-column flex-shrink-0 p-3 text-white bg-dark" style="width: 280px;">
+        <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none">
+            <svg class="bi me-2" width="40" height="32"><use xlink:href="#bootstrap"></use></svg>
+            <span class="fs-4">Sidebar</span>
+        </a>
+        <hr>
+        <ul class="nav nav-pills flex-column mb-auto">
+            <li class="nav-item">
+                <a href="Menu.php" class="nav-link active" aria-current="page">
+                    <svg #id="pag" class="bi me-2" width="16" height="16"><use xlink:href="#home"></use></svg>
+                    Home
+                </a>
+            </li>
+            <li>
+                <a href="listarCliente.php" class="nav-link text-white">
+                    <svg #id="pag" class="bi me-2" width="16" height="16"><use xlink:href="#speedometer2"></use></svg>
+                    Lista de Clientes
+                </a>
+            </li>
+            <li>
+                <a href="InserirCliente.php" class="nav-link text-white">
+                    <svg class="bi me-2" width="16" height="16"><use xlink:href="#table"></use></svg>
+                    Cadastrar Clientes
+                </a>
+            </li>
+            <li>
+                <a href="atualizarCliente.php" class="nav-link text-white">
+                    <svg class="bi me-2" width="16" height="16"><use xlink:href="#grid"></use></svg>
+                    Atualizar Clientes
+                </a>
+            </li>
+            <li>
+                <a href="deletarCliente.php" class="nav-link text-white">
+                    <svg class="bi me-2" width="16" height="16"><use xlink:href="#people-circle"></use></svg>
+                    Deletar Clientes
+                </a>
+            </li>
+        </ul>
+        <hr>
+    </div>
+</div>
+<footer id="rodape" class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
+    <div class="col-md-4 d-flex align-items-center"> <a href="/"
+    class="mb-3 me-2 mb-md-0 text-body-secondary text-decoration-none lh-1" aria-label="Bootstrap"> <svg
+    class="bi" width="30" height="24" aria-hidden="true">
+    <use xlink:href="#bootstrap"></use>
+    <hr>
+</svg> </a> <span id= class="mb-3 mb-md-0 text-body-secondary">© 2025 Company, Inc</span> </div>
+    
+</svg></a></li>
+</ul>
+</footer>
 </body>
 </html>
