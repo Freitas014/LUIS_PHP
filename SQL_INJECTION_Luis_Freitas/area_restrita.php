369df956b829d3cb0281092c6c4915870846b0b2
Luís Fernando de Freitas <freitasluisito508@gmail.com>
2025-07-31 17:03:06 -0300

    Create area_restrita.php
    Teste de SQL Injection

---
+++ SQL_INJECTION_Luis_Freitas/area_restrita.php
@@ -0,0 +1,10 @@
+<?php
+    echo "<h1> &#128680🚨 Área Restrita 🚨&#128680</h1>";
+
+    echo "<p>Você não deveria estar aqui, mas conseguiu acessar devido a uma falha de segurança!</p>";
+
+    echo "<p>Isso mostra como ataques SQL Injection pode compormeter sistemas sem proteção adequada.</p>";
+?>
+<br><br><address align="center">
+        Luís Fernando / Estudante / Técnico Desenvolvimento de Sistemas
+    </address>
