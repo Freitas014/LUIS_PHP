524c8cc22d5c0c370abc67e50663c360f589be7f
Luís Fernando de Freitas <freitasluisito508@gmail.com>
2025-07-31 17:02:26 -0300

    Update pesquisarCliente.php
    Ajustes no código

--- conexao/conexaoPdo/pesquisarCliente.php
+++ conexao/conexaoPdo/pesquisarCliente.php
@@ -1,3 +1,13 @@
+    <!DOCTYPE html>
+    <html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Pesquisar Cliente</title>
+        <link rel="stylesheet" href="style_menu.css">
+    </head>
+    <body>
+    <main>
 <?php
 require_once 'conexao.php';
 
@@ -9,9 +19,9 @@ if(!$busca) {
     ?>
     <form action="pesquisarCliente.php" method="GET">
         <label for="busca">Digite o ID ou Nome: </label>
-        <input type="text" id="busca" name="busca" required>
+        <input class="form-control" type="text" id="busca" name="busca" required>
 
-        <button type="submit">Pesquisar</button>
+        <button id="btn" class="btn btn-primary" type="submit">Pesquisar</button>
     </form>
 <?php
     exit;
@@ -31,26 +41,36 @@ if(!$busca) {
         die("Erro: Nehum cliente encontrado com o ID ou Nome informado.");
     }
     ?>
-    <table border="1">
-        <tr>
-            <th>ID</th>
-            <th>Nome</th>
-            <th>Endereço</th>
-            <th>Telefone</th>
-            <th>E-mail</th>
-            <th>Ação</th>
-        </tr>
-    
-    <?php foreach ($clientes as $cliente): ?>
-        <tr>
-            <td><?=htmlspecialchars($cliente['id_cliente']) ?></td>
-            <td><?=htmlspecialchars($cliente['nome']) ?></td>
-            <td><?=htmlspecialchars($cliente['endereco']) ?></td>
-            <td><?=htmlspecialchars($cliente['telefone']) ?></td>
-            <td><?=htmlspecialchars($cliente['email']) ?></td>
-            <td>
-                <a href=atualizarCliente.php?id=<?=$cliente['id_cliente']?>">Editar</a>
-            </td>
-        </tr>
-        <?php endforeach; ?>
-    </table>
+        <table class="table">
+            <tr>
+                <th>ID</th>
+                <th>Nome</th>
+                <th>Endereço</th>
+                <th>Telefone</th>
+                <th>E-mail</th>
+                <th>Ação</th>
+            </tr>
+        
+        <?php foreach ($clientes as $cliente): ?>
+            <tr>
+                <td><?=htmlspecialchars($cliente['id_cliente']) ?></td>
+                <td><?=htmlspecialchars($cliente['nome']) ?></td>
+                <td><?=htmlspecialchars($cliente['endereco']) ?></td>
+                <td><?=htmlspecialchars($cliente['telefone']) ?></td>
+                <td><?=htmlspecialchars($cliente['email']) ?></td>
+                <td>
+                    <a href="atualizarCliente.php?id=<?=$cliente['id_cliente']?>">Editar</a>
+                </td>
+            </tr>
+            <?php endforeach; ?>
+        </table>
+    </main>
+    <footer id="rodape" class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
+            <div class="col-md-4 d-flex align-items-center"> <a href="/"
+            class="mb-3 me-2 mb-md-0 text-body-secondary text-decoration-none lh-1" aria-label="Bootstrap"> <svg
+            class="bi" width="30" height="24" aria-hidden="true">
+            <use xlink:href="#bootstrap"></use>
+            <hr>
+        </svg> </a> <span id= class="mb-3 mb-md-0 text-body-secondary">© 2025 Company, Inc - Luís Fernando</span> </div>
+    </body>
+    </html>
