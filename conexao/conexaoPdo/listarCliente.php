9e7c5bcc85b6823645234161a7c0acc90cf79ead
Luís Fernando de Freitas <freitasluisito508@gmail.com>
2025-07-31 17:02:22 -0300

    Update listarCliente.php
    Ajustes no código

--- conexao/conexaoPdo/listarCliente.php
+++ conexao/conexaoPdo/listarCliente.php
@@ -85,7 +85,7 @@ $clientes = $stmt -> fetchAll();
     class="bi" width="30" height="24" aria-hidden="true">
     <use xlink:href="#bootstrap"></use>
     <hr>
-</svg> </a> <span id= class="mb-3 mb-md-0 text-body-secondary">© 2025 Company, Inc</span> </div>
+</svg> </a> <span id= class="mb-3 mb-md-0 text-body-secondary">© 2025 Company, Inc - Luís Fernando</span> </div>
     
 </svg></a></li>
 </ul>
