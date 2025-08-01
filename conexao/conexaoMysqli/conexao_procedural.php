b3a92dc2c5b5e852f5e96fc3787a948ec211664d
Luís Fernando de Freitas <freitasluisito508@gmail.com>
2025-08-01 15:34:18 -0300

    Create conexao_procedural.php

---
+++ conexao/conexaoMysqli/conexao_procedural.php
@@ -0,0 +1,37 @@
+<?php
+    // Definição das credencias de acesso ao banco de dados
+    $nomeservidor = 'localhost';  // Endereço do servidor MySQL
+    $usuario = "root";  // Nome de usuário de banco
+    $senha = "";    // Senha do banco
+    $bancodedaos = "empresa"; // Nome do banco de dados
+
+    // Criação da conexão com o MySQLi
+    $conn = mysqli_connect($nomeservidor, $usuario, $senha, $bancodedaos);
+
+    //Verificação da conexão 
+    if(!$conn) { //Caso a conexão falhe, interrompe o script e exibe uma mensagem de erro
+        die("Conexão falhou: " . mysqli_connect_error());
+        }
+
+    // Configuração do conjunto de caracteres para evitar problemas de acentuação
+
+    mysqli_set_charset($conn, "utf8mb4");
+
+    // Mensagem indicando que a conexão foi estabelecida corretamente
+    echo "Conexão bem-sucedida!";
+
+    // Consulta SQL para obter clientes
+    $sql = "SELECT id_cliente, nome, email FROM cliente";
+    $result = mysqli_query($conn, $sql);
+
+    // Verifica se há resultados na consulta
+    if (mysqli_num_rows($result) > 0) {
+        // Itera sobre os resultados e exibe os dados
+        while($linha = mysqli_fetch_assoc($result)) { // mysqli_fetch_assoc não deixa trazer índice repetido
+            echo "ID: <br>" . $linha["id_cliente"] . " - Nome: " . $linha["nome"] . " - Email: " . $linha["email"] . "<br>";}
+        } else {
+            echo "Nehum resultado encontrado.";}
+        
+    // Fecha a conexão com o banco de dados
+    mysqli_close($conn);
+?>
