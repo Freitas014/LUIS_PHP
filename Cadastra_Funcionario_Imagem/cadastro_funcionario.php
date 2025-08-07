8408bc3b58d7d6888def22f253301362b1f96402
Luís Fernando de Freitas <freitasluisito508@gmail.com>
2025-08-07 16:57:02 -0300

    Update cadastro_funcionario.php
    adicionado adress com meu nome

--- Cadastra_Funcionario_Imagem/cadastro_funcionario.php
+++ Cadastra_Funcionario_Imagem/cadastro_funcionario.php
@@ -13,19 +13,22 @@
         <!-- Formulário para cadastrar um funcionário -->
     <form action="salvar_funcionario.php" method="POST" enctype="multipart/form-data">
         <!-- Campo para inserir o nome do funcionario-->
-        <label for="nome">Nome: </label>
+        <label for="nome">Nome: </label><br>
         <input type="text" name="nome" id="nome" required><br>
         
         <!-- Campo para inserir o telefone do funcionario-->
-        <label for="telefone">Telefone: </label>
+        <label for="telefone">Telefone: </label><br>
         <input type="text" name="telefone" id="telefone" required><br>
 
         <!-- Campo para fazer upload da foto do funcionario-->
-        <label for="foto">Foto: </label>
+        <label for="foto">Foto: </label><br>
         <input type="file" name="foto" id="foto" required><br>
 
         <button type="submit">Cadastrar</button>
     </form>
     </div>
+    <br><br><address align="center">
+        Luís Fernando / Estudante / Técnico Desenvolvimento de Sistemas
+    </address>
 </body>
 </html>
